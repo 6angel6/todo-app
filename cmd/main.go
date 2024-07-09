bf2ae3c8820e7fa538d7a07b49e0d606ee266ee7
r_angel <143842574+6angel6@users.noreply.github.com>
2024-07-09 16:08:10 +0500

    added db, migrations for db and env

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,46 @@ import (
 	"TODOapp/pkg/repository"
 	"TODOapp/pkg/service"
 	todo "TODOapp/server"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
-	repos := repository.NewRepository()
+
+	if err := initConfig(); err != nil {
+		log.Fatal(err)
+	}
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatal("Error to init fb", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	server := new(todo.Server)
-	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("server.port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error starting server: %v", err)
 	}
+
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	return viper.ReadInConfig()
 }
