fbe4c81f9b610f1c8a4a4d26113d70fb463b7c87
r_angel <143842574+6angel6@users.noreply.github.com>
2024-07-12 23:05:44 +0500

    added repositories

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,14 +1,29 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"TODOapp/model"
+	"github.com/jmoiron/sqlx"
+)
 
 type Authorization interface {
+	CreateUser(user model.User) (int, error)
+	GetUser(username, password string) (model.User, error)
 }
 
 type ToDoList interface {
+	Create(userId int, list model.TodoList) (int, error)
+	GetAll(userId int) ([]model.TodoList, error)
+	GetById(userId int, listId int) (model.TodoList, error)
+	Delete(userId, listId int) error
+	Update(userId, listId int, input model.UpadteListInput) error
 }
 
 type ToDoItem interface {
+	Create(listId int, item model.TodoItem) (int, error)
+	GetAll(userId int, listId int) ([]model.TodoItem, error)
+	GetById(userId, itemId int) (model.TodoItem, error)
+	Delete(userId, itemId int) error
+	Update(userId, itemId int, input model.UpadteItemInput) error
 }
 
 type Repository struct {
@@ -18,5 +33,9 @@ type Repository struct {
 }
 
 func NewRepository(db *sqlx.DB) *Repository {
-	return &Repository{}
+	return &Repository{
+		Authorization: NewAuthPostgres(db),
+		ToDoList:      NewTodoListPostgres(db),
+		ToDoItem:      NewItemPostgres(db),
+	}
 }
