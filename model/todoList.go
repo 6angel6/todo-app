bc55ff9f97ec195a23f9d7ed80544427acbfe072
r_angel <143842574+6angel6@users.noreply.github.com>
2024-07-12 23:06:42 +0500

    changed models

--- model/todoList.go
+++ model/todoList.go
@@ -1,8 +1,10 @@
 package model
 
+import "errors"
+
 type TodoList struct {
 	Id          int    `json:"id"`
-	Title       string `json:"Title"`
+	Title       string `json:"Title" binding:"required"`
 	Description string `json:"Description"`
 }
 
@@ -13,14 +15,31 @@ type UserList struct {
 }
 
 type TodoItem struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Done        bool   `json:"done"`
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Description string `json:"description" db:"description"`
+	Done        bool   `json:"done" db:"done"`
 }
 
 type List struct {
 	Id     int
 	ListId int
 	ItemId int
+}
+type UpadteListInput struct {
+	Title       *string `json:"Title"`
+	Description *string `json:"Description"`
+}
+
+type UpadteItemInput struct {
+	Title       *string `json:"Title"`
+	Description *string `json:"Description"`
+	Done        *bool   `json:"Done"`
+}
+
+func (i UpadteItemInput) Validate() error {
+	if i.Title == nil && i.Description == nil && i.Done == nil {
+		return errors.New("update structure has no values")
+	}
+	return nil
 }
